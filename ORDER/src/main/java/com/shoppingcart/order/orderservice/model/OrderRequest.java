01f1681df10b67f4ff3028c9e6e4177f92359962
mehedee-hassan <mehedee.hassan@outlook.com>
2023-10-08 23:33:18 +0900

    adding builder

--- ORDER/src/main/java/com/shoppingcart/order/orderservice/model/OrderRequest.java
+++ ORDER/src/main/java/com/shoppingcart/order/orderservice/model/OrderRequest.java
@@ -8,12 +8,54 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+
 public class OrderRequest {
+
+
+    public OrderRequest(Builder builder){
+        this.productId = builder.productId;
+    }
 
     private long productId;
     private long totalAmount;
     private long quantity;
     private PaymentMode paymentMode;
+
+    //setting up builder pattern
+    public static class Builder{
+
+        private long productId;
+        private long totalAmount;
+        private long quantity;
+        private PaymentMode paymentMode;
+
+        public static Builder newInstance(){
+            return new Builder();
+        }
+
+        private Builder(){
+            // close to instantiate
+        }
+
+        public Builder productId(long productId){
+            this.productId = productId;
+            return this;
+        }
+
+        public Builder totalAmount(long totalAmount){
+            this.totalAmount = totalAmout;
+            return this;
+        }
+
+        public void quantity(long quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public void paymentMode(PaymentMode paymentMode) {
+            this.paymentMode = paymentMode;
+            return this;
+        }
+    }
 
 }
