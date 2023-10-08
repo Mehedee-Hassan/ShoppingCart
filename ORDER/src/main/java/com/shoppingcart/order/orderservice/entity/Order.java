e970506c8e1471e127b49ff90c8c3db6cdc16762
mehedee-hassan <mehedee.hassan@outlook.com>
2023-10-08 23:04:36 +0900

    order model remove lombork

--- ORDER/src/main/java/com/shoppingcart/order/orderservice/entity/Order.java
+++ ORDER/src/main/java/com/shoppingcart/order/orderservice/entity/Order.java
@@ -1,10 +1,7 @@
 package com.shoppingcart.order.orderservice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 import java.lang.reflect.GenericArrayType;
 import java.time.Instant;
@@ -12,10 +9,69 @@ import java.time.Instant;
 @Entity
 @Table(name="ORDER_DETAILS")
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class Order {
+
+    public Order(){
+
+    }
+
+    public Order(long id, long productId, long quantity, Instant orderDate, String orderStatus, long amount) {
+        this.id = id;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.amount = amount;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public Instant getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Instant orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
